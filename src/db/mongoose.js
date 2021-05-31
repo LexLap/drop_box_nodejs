6805ffe3d5a8a748ce19085efc86dda8e696b680
LexaLap <alexeyint@gmail.com>
2021-06-01 01:18:05 +0300

    first commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false
-// });
-
-let MONGODB_URL = `mongodb+srv://user:Bezeqint1@cluster0.okpxi.mongodb.net/drop-box-aws?retryWrites=true&w=majority`
-
-mongoose.connect(MONGODB_URL, {
+mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true 
-})
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+// let MONGODB_URL = `mongodb+srv://user:Bezeqint1@cluster0.okpxi.mongodb.net/drop-box-aws?retryWrites=true&w=majority`
+
+// mongoose.connect(MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true 
+// })
